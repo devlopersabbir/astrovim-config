e6d8fc90657a404b0cb657ecb393cf54a0e63b88
Sabbir Hossain Shuvo <82939905+devlopersabbir@users.noreply.github.com>
2023-08-06 16:04:27 +0600

    Update community plugin

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,6 +4,17 @@ return {
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  {import = "astrocommunity.pack.html-css"},
+  {import = "astrocommunity.pack.json"},
+  {import = "astrocommunity.pack.markdown"},
+  {import = "astrocommunity.pack.php"},
+  {import = "astrocommunity.pack.prisma"},
+  {import = "astrocommunity.pack.python"},
+  {import = "astrocommunity.pack.tailwindcss"},
+  {import = "astrocommunity.pack.typescript"},
+  {import = "astrocommunity.pack.typescript-all-in-one"}
+ 
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
 }
